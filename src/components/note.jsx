af062eb622d8b332e0f39d2da437cf0f5f238e7f
kittu-20 <sanyam.student.mec19@itbhu.ac.in>
2021-03-17 03:49:31 +0530

    initial commit

---
+++ src/components/note.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Note(){
+    return (
+    <div className="note">
+        <h1>Heading</h1>
+        <p>Content ...</p>
+    </div>
+        
+    );
+}
+
+export default Note;
